1f42c83770d6a00a7be564fffebe6a30640585a4
jameskimmel <17176225+jameskimmel@users.noreply.github.com>
2023-02-14 23:35:29 +0100

    Version 1.4

--- README.md
+++ README.md
@@ -4,16 +4,17 @@ Unfortunately BIMCloud Basic has no backup option included.
 
 That is why I wrote my own script, based on the v24 backup script from Graphisoft:
 http://dl.graphisoft.com/ftp/techsupport/documentation/BIMcloud_Backup_Guide/v24PcServerAndManagerBk...
-I was unable to find a newer official template for v25 or v26. The script was tested to work with v26. 
+I was unable to find a newer official template. Please let me know if you spot a new one.
+The script was tested (14.02.2023) to work with with BIMCloud 2022.1.
 
 The script is just a single backup.bat batch file. 
 
 Please carefully read the comments in the script. Every line that starts with "REM" is a comment and not code. Adjust the paths and services accordingly. 
-Remember to always test the script and make sure it really works. I don't take any responsibility. If you need technical support or have questions you find contact information on www.salzmann.solutions
-
+Be carefull to read the fine print during the BIMCloud installation, if you have BIMCloud basic, do NOT set a Backup destination path during the installation!
 Like the original script vom ArchiCAD, this script only creates a single backup state. If you want multiple backup days/states, you should consider some kind of file versioning or snapshots of your backup destination. 
+Remember to always test the script and make sure it really works. I don't take any responsibility. If you need technical support or have questions you find contact information on www.salzmann.solutions
 
 Requirements that are out of the scope of this repository:
 
-- Windows task scheduler to automatically start this script in an interval
-- Windows Services to get the service names of Graphisoft to start and stop them
+- Windows task scheduler to automatically start this script automatically 
+- File versioning or snapshots to have multiple backup states
--- backup.bat
+++ backup.bat
@@ -1,37 +1,40 @@
 @echo off
 
-REM Version 1.3
-REM Adjust path for the below components. Projects, Libraries, and Manager Data folders can be assigned to special locations! The Major build number can change after updating BIMcloud! This example here
-REM has the installation date 2022-01-10. You can copy this file and adjust the ServerInstallDate below to create another backup of another instance.
+REM Version 1.4
 
-REM It is very important, that you don't blindly trust this script to work and test it! Look if it really created all the backup files. You also need to check this after every update!
-REM Per default, this script only creates one backup state. If you wanna save multiple days or states, you should consider using some kind of file versioning or snapshots.
+REM Adjust path for the below components. Projects, Libraries, and Manager Data folders all have different locations! The services name can change after updating BIMcloud! That is why
+REM you have to check this script after every Update!
+REM It is very important, that you don't blindly trust this script to work and test it! Check if it really created a backup of all files.
+REM Per default, this script only creates one backup state. If you wanna save multiple days or states, you should consider using some kind of OS file versioning or snapshots.
 REM If you need technical support or have questions you find contact information on www.salzmann.solutions
 
-REM First we set the Server installation date. This date defines all the paths Graphisoft uses. In this example, the date is 2022-01-10
-set ServerInstallDate=2022-01-10
+REM This example here has the installation date 2023-02-14. You can dublicate this file to backup multiple instances of BIMCloudBasic. Just remember to set all paths accordingly.
 
+REM First we set the Server installation date. This variable is used to set all Graphisoft paths accordingly. In this example, the date is 2023-02-14. Adjust it according to your installation. 
+set ServerInstallDate=2023-02-14
+
+REM Next we set the backup path.
+REM Caution! The Backup Directory has to already exist as an empty folder. The script will not create it and fail if it does not already exists.
+set localBkUp=D:\BackupPath\BimserverBackup\Server-%ServerInstallDate%
+
+REM Next we set names of the services.
+REM Open "services" and go to the "Graphisoft" services. You can double click on them to open them and look up the name under "Service name". 
+REM The BIMCloud naming is not the same as the Archicad naming. By February 2023 the current version is called BimCloud 2022.1 and the services end with V26. 
+REM Caution! These services can change with every BimCloud Update! 
+set Manager=PortalServerService-v26.0(Manager-%ServerInstallDate%)
+set Server=TeamworkApplicationServerMonitor-v26.0(Server-%ServerInstallDate%)
+
+REM set a delay for starting and stopping services, depending on how fast our Server can star and stop services.
+set delay=60
+
+REM Editing the script below should not be necessary
+
+REM Because we previously set the install date as a %ServerInstallDate% variable, we pupulate the other paths automatically.
 set ManagerDir=C:\Program Files\GRAPHISOFT\BIMcloud\Manager-%ServerInstallDate%
 set ManagerDataDir=C:\Program Files\GRAPHISOFT\BIMcloud\Manager-%ServerInstallDate%\Data
 set ServerDir=C:\Program Files\GRAPHISOFT\BIMcloud\Server-%ServerInstallDate%
 set ProjectDir=C:\Program Files\GRAPHISOFT\BIMcloud\Server-%ServerInstallDate%\Projects
 set LibraryDir=C:\Program Files\GRAPHISOFT\BIMcloud\Server-%ServerInstallDate%\Attachments
-
-REM Caution! The Backup Directory has to already exist as an empty folder. The script will not create it and fail if it does not exist.
-set localBkUp=D:\BackupPath\BimserverBackup\Server-%ServerInstallDate%
-
-REM Caution! These services can change with every BimCloud Update! You have to look it up in the Windows "services" application. Even if you use this script
-REM To backup BimCloud 24, BIMCloud could be on a higher Version and the service name reflects that Version. By January 2022 the current version is v26. With the next update, this could change to v27!
-REM I added additional services to the script to stop all Graphisoft Services
-
-set Manager=PortalServerService-v26.0(Manager-%ServerInstallDate%)
-set Server=TeamworkApplicationServerMonitor-v26.0(Server-%ServerInstallDate%)
-
-
-REM set the delay between Services stopping and starting. Depending on how fast our Server is and how fast it can robocopy the files to the backup destination
-set delay=60
-
-REM Editing the script below should not be necessary
 
 REM Checking if BIMcloud Basic or the Backup folder does not exist. Creating subfolders in the Backup folder.
 if not exist "%ManagerDir%" echo Missing installation directory "%ManagerDir%"
@@ -56,39 +59,35 @@ if not exist "%localBkUp%\Manager" goto done
 
 REM Stopping Server and Manager and Service Process Manager
 sc stop "%Manager%" 
-ping localhost -n %delay% >nul
+ping localhost -n %delay%
 
 sc stop "%Server%" 
-ping localhost -n %delay% >nul
+ping localhost -n %delay%
 
 REM The original Backup routine from Graphisoft deleted all folders and then copied over all files. 
 REM In my opinion, this is unnecessary writes. Instead of deleting everything and rewriting it afterward, we can mirror the source to our backup destination. 
 REM with the mirror option, only changed files need to be rewritten. Deleted files in the source also get deleted in the backup destination. 
 
-REM Copying the Manager's data
+REM Copying the Managers data
 robocopy "%ManagerDataDir%" "%localBkUp%\Manager\Data" /MIR /R:10 /W:3
 robocopy "%ManagerDir%\Config" "%localBkUp%\Manager\Config" /MIR /R:10 /W:3
 
-REM Copying the Server's data
+REM Copying the Servers data
 robocopy "%ServerDir%\Config" "%localBkUp%\Server\Config" /MIR /R:10 /W:3
 robocopy "%ServerDir%\Mailboxes" "%localBkUp%\Server\Mailboxes" /MIR /R:10 /W:3
 robocopy "%ServerDir%\Sessions" "%localBkUp%\Server\Sessions" /MIR /R:10 /W:3
 robocopy "%ProjectDir%" "%localBkUp%\Server\Projects" /MIR /R:10 /W:3
 robocopy "%LibraryDir%" "%localBkUp%\Server\Attachments" /MIR /R:10 /W:3
 
-
-
 REM If you wanna reboot the server instead of starting the services, delete the "REM" in the next two lines. The services start on their own on boot.
 REM shutdown /r /t 10
 REM :done
 
-
-REM Restarting Server service with 60 seconds delay
+REM Restarting Server services with delay
 sc start "%Manager%"
-ping localhost -n %delay% >nul
+ping localhost -n %delay%
 
 sc start "%Server%"
-ping localhost -n %delay% >nul
-
+ping localhost -n %delay%
 
 :done
