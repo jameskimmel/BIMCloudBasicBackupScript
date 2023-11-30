677cb181bf1fdf36ee1bc9f9cf86e2639acb1bcd
jameskimmel <17176225+jameskimmel@users.noreply.github.com>
2023-12-01 00:04:28 +0100

    V28 Update

--- backup.bat
+++ backup.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Version 1.5
+REM Version 1.6
 
 REM Adjust path for the below components. Projects, Libraries, and Manager Data folders all have different locations! The services name can change after updating BIMcloud! That is why
 REM you have to check this script after every Update!
@@ -8,24 +8,24 @@ REM It is very important, that you don't blindly trust this script to work and t
 REM Per default, this script only creates one backup state. If you wanna save multiple days or states, you should consider using some kind of OS file versioning or snapshots.
 REM If you need technical support or have questions you find contact information on www.salzmann.solutions
 
-REM This example here has the installation date 2023-02-14. You can dublicate this file to backup multiple instances of BIMCloudBasic. Just remember to set all paths accordingly.
+REM This example here has the installation date 2023-11-30. You can dublicate this file to backup multiple instances of BIMCloudBasic. Just remember to set all paths accordingly.
 
-REM First we set the Server installation date. This variable is used to set all Graphisoft paths accordingly. In this example, the date is 2023-02-14. Adjust it according to your installation. 
-set ServerInstallDate=2023-02-14
+REM First we set the Server installation date. This variable is used to set all Graphisoft paths accordingly. In this example, the date is 2023-11-30. Adjust it according to your installation. 
+set ServerInstallDate=2023-11-30
 
 REM Next we set the backup path.
 REM Caution! The Backup Directory has to already exist as an empty folder. The script will not create it and fail if it does not already exists.
-set localBkUp=D:\BackupPath\BimserverBackup\Server-%ServerInstallDate%
+set localBkUp=D:\BimBackup\Server-%ServerInstallDate%
 
 REM Next we set names of the services.
 REM Open "services" and go to the "Graphisoft" services. You can double click on them to open them and look up the name under "Service name". 
-REM The BIMCloud naming is not the same as the ArchiCAD naming! By May 2023 the current version is called BimCloud 2023.1 and the services end with V27. 
+REM The BIMCloud naming is not the same as the ArchiCAD naming! By November 2023 the current version is called BimCloud 2023.3 and the services end with V28. 
 REM Caution! These services can change with every BimCloud Update! 
-set Manager=PortalServerService-v27.0(Manager-%ServerInstallDate%)
-set Server=TeamworkApplicationServerMonitor-v27.0(Server-%ServerInstallDate%)
+set service1=PortalServerService-v28.0(Manager-%ServerInstallDate%)
+set service2=TeamworkApplicationServerMonitor-v28.0(Server-%ServerInstallDate%)
 
 REM set a delay for starting and stopping services, depending on how fast our Server can star and stop services.
-set delay=60
+set delay=20
 
 REM Editing the script below should not be necessary
 
@@ -36,7 +36,7 @@ set ServerDir=C:\Program Files\GRAPHISOFT\BIMcloud\Server-%ServerInstallDate%
 set ProjectDir=C:\Program Files\GRAPHISOFT\BIMcloud\Server-%ServerInstallDate%\Projects
 set LibraryDir=C:\Program Files\GRAPHISOFT\BIMcloud\Server-%ServerInstallDate%\Attachments
 
-REM Checking if BIMcloud Basic or the Backup folder does not exist. Creating subfolders in the Backup folder.
+REM Checking if BIMcloud Basic or the Backup folder does not exist. You must create them beforehand!
 if not exist "%ManagerDir%" echo Missing installation directory "%ManagerDir%"
 if not exist "%ManagerDataDir%" echo Missing installation directory "%ManagerDataDir%"
 if not exist "%ServerDir%" echo Missing installation directory "%ServerDir%"
@@ -57,11 +57,11 @@ if not exist "%localBkUp%\Manager" md "%localBkUp%\Manager"
 if not exist "%localBkUp%\Server" goto done
 if not exist "%localBkUp%\Manager" goto done
 
-REM Stopping Server and Manager and Service Process Manager
-sc stop "%Manager%" 
+REM Stopping services
+sc stop "%service1%" 
 ping localhost -n %delay%
 
-sc stop "%Server%" 
+sc stop "%service2%" 
 ping localhost -n %delay%
 
 REM The original Backup routine from Graphisoft deleted all folders and then copied over all files. 
@@ -84,10 +84,10 @@ REM shutdown /r /t 10
 REM :done
 
 REM Restarting Server services with delay
-sc start "%Manager%"
+sc start "%service1%"
 ping localhost -n %delay%
 
-sc start "%Server%"
+sc start "%service2%"
 ping localhost -n %delay%
 
 :done
